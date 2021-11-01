a217f77bf5ddc13c3c86fa6f4428c288b848f083
KaLen9ula <andruhovickola167@gmail.com>
2021-11-01 11:26:52 +0200

    refactor game requirements

--- game/console.rb
+++ game/console.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'loader'
-require 'pry'
 
 class Console
   include ConsoleGame
--- game/loader.rb
+++ game/loader.rb
@@ -3,7 +3,7 @@
 require 'codebraker'
 require 'i18n'
 require 'terminal-table'
-require_relative 'console'
 require_relative 'view'
+require_relative 'console'
 I18n.load_path << Dir[['config', 'locales', '**', '*.yml'].join('/')]
 I18n.config.available_locales = :en
